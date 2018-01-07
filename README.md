57b30fa3f2b79f3edecab7a556f968beab71aba2
Sbakken21 <sbakken21@gmail.com>
2018-01-07 04:33:13 -0600

    added README

---
+++ README.md
@@ -0,0 +1,39 @@
+#CryptoTracker
+This is a python script that utilizes the coinmarketcap API in order to monitor the prices of numerous cryptocurrencies at once. If the user selected values are reached the script will send an email notification to alert the user that it is either time to buy or sell the selected currencies.
+##Getting started
+###Prerequisites
+This script uses python 3.6.
+
+Use the included requirements.txt:
+```
+pip install -r requirements.txt
+```
+###User Setup
+ In `config.example.cfg` change the values to indicate the coins you wish to monitor, and set the prices at which you would like the coins to reach in order to sell and buy them. 
+ 
+ *Note: do NOT use commas when inputting the values.* 
+ 
+ * set `from_address` to the email you are using to send this script from
+ * set `password` to the password for the email used for `from_address`
+ * set `to_address` to the email that you wish the notifications to be sent (this can be the same as the `from_address`)
+  
+Once all the values are entered, save `config.example.cfg` as `config.cfg`.
+  
+ ####Quick Example of config.cfg
+ ```
+[watch]
+
+tickers_list = Bitcoin, Ark, Ripple
+sell_price = 20000.00, 8.00, 5.00
+buy_price = 15000.00, 2.00, 2.00
+
+[email]
+
+from_address = bob@gmail.com
+password = ***********
+to_address = joe@gmail.com
+```
+ 
+
+ 
+ 
--- main.py
+++ main.py
@@ -9,39 +9,37 @@ def get_ticker_info(coinmarketcap, tickers_list, sell_price, buy_price):
     """Return coin information"""
 
     # Obtains all info from each ticker on the declared tickers list
-    for ticker, low_price, high_price in zip(tickers_list, sell_price, buy_price):
+    for ticker, sell_value, buy_value in zip(tickers_list, sell_price, buy_price):
         info = coinmarketcap.ticker(currency=ticker)[0]
         id = info.get('id')
         price_usd = info.get('price_usd')
         print('ID: %s\n' %id + 'Price (USD): %s' %price_usd)
-        compare_info(price_usd, low_price, high_price, id)
+        compare_info(price_usd, sell_value, buy_value, id)
         print('=====' * 10)
 
-def compare_info(price_usd, low_price, high_price, id):
+def compare_info(price_usd, selling, buying, id):
     """Compares the coin's current price and target price """
 
-    # This is the target price for bitcoins to be bought
-    if float(low_price) <= float(price_usd):
-        print('This bitcoin is at the target price to buy')
-        notification(id, price_usd)
-    elif float(high_price) >= float(price_usd):
+    # This is the target price for bitcoins to be bought and sold
+    if float(selling) <= float(price_usd):
         print('This bitcoin is at the target price to sell')
+        notification(id, price_usd)
+    elif float(buying) >= float(price_usd):
+        print('This bitcoin is at the target price to buy')
         notification(id, price_usd)
     else:
         print('not at target price yet')
 
-
-
-
 def notification(id, price):
-    """Send phone notification that target price has been reached"""
+    """Send email notification that target price has been reached"""
 
     # load config
     config = get_config()
 
     # Create Email notification
-    from_address = config.get("email", "username")
+    from_address = config.get("email", "from_address")
     to_address = config.get("email", "to_address")
+    password = config.get("email", "password")
     message = MIMEMultipart()
     message['From'] = from_address
     message['To'] = to_address
@@ -54,7 +52,7 @@ def notification(id, price):
     server.starttls()
     server.login(
         from_address,
-        config.get("email", "password")
+        password
     )
     text = message.as_string()
     server.sendmail(from_address, to_address, text)
@@ -86,6 +84,6 @@ if __name__=="__main__":
     while True:
         try:
             main()
-            time.sleep(600)
+            time.sleep(600) # Interval in seconds that script updates
         except KeyboardInterrupt:
             break
